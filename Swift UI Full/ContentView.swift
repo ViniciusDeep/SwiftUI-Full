a1eb9099cd024144b55306dc5c9abdfdb34aafe9
ViniciusDeep <vinicius.correia@popcode.com.br>
2020-02-24 11:35:09 -0300

    Initial Commit

---
+++ Swift UI Full/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Full
+//
+//  Created by Vinicius Mangueira on 24/02/20.
+//  Copyright © 2020 Vinicius Mangueira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
