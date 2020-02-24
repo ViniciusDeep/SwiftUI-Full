3fc190a1b98993607ea0ae03b0e76c68d48fb083
ViniciusDeep <vinicius.correia@popcode.com.br>
2020-02-24 11:37:08 -0300

    Launch project 🚀

--- Swift UI Full/ContentView.swift
+++ Swift UI Full/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            Text("Hello, World!")
+            Text("Here we go again")
+        }
     }
 }
 
